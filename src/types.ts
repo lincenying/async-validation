5e54dd90e3a56159030a9d5f794ea4550de63994
林岑影 <lincenying@qq.com>
2023-11-13 10:37:59 +0800

    chore: update tsconfig

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,32 @@
 /**
- * Null 或者 T
+ * Null 或者 Undefined 或者 T
  */
-export type Nullable<T> = T | null
+export type Nullable<T> = T | null | undefined
 /**
- * Undefined 或者 T
+ * 非 Null 类型
  */
-export type UnfAble<T> = T | undefined
+export type NonNullable<T> = T extends null | undefined ? never : T
+/**
+ * 数组<T> 或者 T
+ */
+export type Arrayable<T> = T | T[]
 /**
  * 键为字符串, 值为 Any 的对象
  */
 export type Obj = Record<string, any>
-
 /**
  * 键为字符串, 值为 T 的对象
  */
 export type ObjT<T> = Record<string, T>
-
-export type AnyFn = (...args: any[]) => any
+/**
+ * Function
+ */
+export type Fn<T = void> = () => T
+/**
+ * 任意函数
+ */
+export type AnyFn<T = any> = (...args: any[]) => T
+/**
+ * Promise, or maybe not
+ */
+export type Awaitable<T> = T | PromiseLike<T>
